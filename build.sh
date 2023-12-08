30d6669e199ae523485b5f7e5de064c80aad765e
Remi MERGEN <remi.mergen@epitech.eu>
2023-12-08 17:15:46 +0100

    feat: WIP vcpkg implementation

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Build the project
-cmake -S . -B "./build/" || exit
+cmake -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake -S . -B "./build/" || exit
 
 cd "./build/" || exit
 
-make -j 20|| exit
+make -j 20
 make package -j 20
 make package_source -j 20
