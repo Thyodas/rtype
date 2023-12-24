60dc3c9b316f8daed0977e47cf3dedcaed2904d3
Thyodas <guillaume.hein@epitech.eu>
2023-12-24 18:32:52 +0100

    feat(compilation-cross-platform): add more exclude of Windows.h

--- include/common/my_raylib.h
+++ include/common/my_raylib.h
@@ -7,5 +7,8 @@
 
 #define NOGDI             // Excludes GDI (Graphics Device Interface) definitions
 #define NOUSER            // Excludes User API definitions
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#define NOGDI               // Exclude GDI definitions
+#define NOUSER              // Exclude User definitions
 
 #include "raylib.h"
