26cda5b5a1528f76898d10261db42489f4894858
Rémi MERGEN <remi.mergen@epitech.eu>
2024-01-10 18:17:34 +0100

    feat (tarball): script to buildj and create .tar

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Build the project
-cmake -B "./build/" || (echo "cmake failed" && exit 1)
+(cmake -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release) || (echo "Build failed" && exit 1)
 
-cmake --build build || (echo "Build failed" && exit 1)
+([[ -e ./build/Release ]] && cp ./build/Release/* ./build) || ([[ -e ./build/Debug ]] && cp ./build/Debug/* ./build)
+
+cp -r ./ressources ./build/ || (echo "Copy ressources failed" && exit 1)
+
+tar -C $(pwd) -cf package.tar build || (echo "Tar failed" && exit 1)
