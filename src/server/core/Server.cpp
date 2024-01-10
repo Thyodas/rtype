e0483f3107f29e13e986e191960300726de10ac8
Thyodas <guillaume.hein@epitech.eu>
2024-01-10 15:22:41 +0100

    fix(client-collision): fixed the collision bug, the spawner had a bounding while it should not

--- src/server/core/Server.cpp
+++ src/server/core/Server.cpp
@@ -23,19 +23,9 @@ void server::Server::run()
 
     // créer un cube et attacher le enemyspawn behavior
     common::game::EntityFactory factory;
-    ecs::Entity cube = factory.createEntity(common::game::ObjectType::Cube, common::game::ObjectName::None, {
-            {0, 0, 0},
-            0,
-            0,
-            0,
-            RED,
-            false,
-            RED,
-            {0, 0, 0},
-            {50, 50, 50}
-    }, common::game::ObjectFormat::F_NONE);
-    auto behave = engine::createBehavior<ecs::components::behaviour::EnemySpawner>(_netServer, cube);
-    engine::attachBehavior(cube, behave);
+    ecs::Entity spawner = engine::createEntity();
+    auto behave = engine::createBehavior<ecs::components::behaviour::EnemySpawner>(_netServer, spawner);
+    engine::attachBehavior(spawner, behave);
     while (true) {
         _netServer.update(-1, true);
         engine::runEngine();
