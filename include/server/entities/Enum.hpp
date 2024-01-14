da55b7447f0317d9cd4ed28fcb9e06e90be605d2
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2024-01-14 16:09:43 +0100

    fix: add missing file

---
+++ include/server/entities/Enum.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-STG-5-2-rtype-remi.mergen
+** File description:
+** Enum.hpp
+*/
+
+namespace server {
+    namespace entities {
+        enum EntityType {
+            PLAYER,
+            ALLY,
+            ENEMY,
+            BULLET,
+            ENTITY_UNDEFINED
+        };
+    }
+}
